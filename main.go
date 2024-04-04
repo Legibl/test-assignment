76d769d7d34191713d9dd7772faae37d46d1d214
Legibl <89272199+Legibl@users.noreply.github.com>
2024-04-04 22:13:34 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,67 +1,68 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    "log"
-    "net/http"
-    _"github.com/go-sql-driver/mysql"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var db *sql.DB
 var err error
 
 func initDB() {
-    db, err = sql.Open("mysql", "user:password@/dbname")
-    if err != nil {
-        log.Fatal(err)
-    }
+	db, err = sql.Open("mysql", "user:password@/dbname")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-err = db.Ping()
-if err != nil {
-    log.Fatal(err)
-    }
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func searchHandler(w http.ResponseWriter, r *http.Request) {
-    if r.Method != "GET" {
-        http.Error(w, "Method is not supported.", http.StatusNotFound)
-        return
-    }
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
 
-searchQuery := r.URL.Query().Get("query")
-if searchQuery == "" {
-    http.Error(w, "Query parameter is missing", http.StatusBadRequest)
-    return
-}
+	searchQuery := r.URL.Query().Get("query")
+	if searchQuery == "" {
+		http.Error(w, "Query parameter is missing", http.StatusBadRequest)
+		return
+	}
 
-query := fmt.Sprintf("SELECT * FROM products WHERE name LIKE '%%%s%%'", searchQuery) // SQL инъекция
-rows, err := db.Query(query) ///
-if err != nil { ///
-    http.Error(w, "Query failed", http.StatusInternalServerError) /// Выдача информации об ошибках
-    log.Println(err) ///
-    return ///
-}
-defer rows.Close()
+	query := "SELECT * FROM products WHERE name LIKE :searchQuery"
+	rows, err := db.Query(query, sql.Named("searchQuery", "%"+searchQuery+"%"))
+	if err != nil {
+		http.Error(w, "Failed to process request", http.StatusInternalServerError)
+		log.Println("Database query error:", err)
+		return
+	}
+	defer rows.Close()
 
-var products []string
-for rows.Next() {
-    var name string
-    err := rows.Scan(&name)
-    if err != nil {
-        log.Fatal(err)
-    }
-    products = append(products, name)
-}
+	var products []string
+	for rows.Next() {
+		var name string
+		err := rows.Scan(&name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		products = append(products, name)
+	}
 
-fmt.Fprintf(w, "Found products: %v\n", products)
+	fmt.Fprintf(w, "Found products: %v\n", products)
 }
 
 func main() {
-    initDB()
-    defer db.Close()
+	initDB()
+	defer db.Close()
 
-http.HandleFunc("/search", searchHandler)
-fmt.Println("Server is running")
-log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/search", searchHandler)
+	fmt.Println("Server is running")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
